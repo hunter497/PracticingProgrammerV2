6b26c085abf636e186442e832181f1fa521a69c0
mbowman8 <romans1.164e@yahoo.com>
2014-07-13 15:40:13 -0400

    Stuff

--- Practicing_Programmer/App_Start/BundleConfig.cs
+++ Practicing_Programmer/App_Start/BundleConfig.cs
@@ -25,7 +25,10 @@ namespace Practicing_Programmer
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Home.css", 
+                      "~/Content/Kata.css",
+                      "~/Content/Question.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
